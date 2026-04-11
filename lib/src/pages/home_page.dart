27518ff98fdadaab677432a764d0029ec1a732b4
chrisdroid <bobitlmr188221@spu.ac.ke>
2026-04-11 20:46:22 +0300

    Set Up Application Shell

---
+++ lib/src/pages/home_page.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:stage_0_mobile/src/screens/currency_converter_screen.dart';
+import 'package:stage_0_mobile/src/screens/journaling_screen.dart';
+import 'package:stage_0_mobile/src/screens/todo_list_screen.dart';
+import 'package:stage_0_mobile/src/widgets/custom_bottom_navigation_bar.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int _pageIndex = 0;
+  void _nextPage(int index) {
+    setState(() {
+      _pageIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      bottomNavigationBar: CustomBottomNavigationBar(
+        pageIndex: _pageIndex,
+        onDestinationSelected: _nextPage,
+      ),
+      body: <Widget>[
+        TodoListScreen(),
+
+        CurrencyConverterScreen(),
+
+        JournalingScreen(),
+      ][_pageIndex],
+    );
+  }
+}
